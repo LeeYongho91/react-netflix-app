447d7c62501f1d7f6e1ba9ae31a8926e73acf0d6
LeeYongho91 <skyks4848@naver.com>
2024-01-15 23:41:02 +0900

    배너 추가

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -26,6 +26,8 @@ export default function Banner() {
     fetchData();
   }, []);
 
+  const truncate = (str, n) => (str?.length > n ? `${str.substr(0, n - 1)}...` : str);
+
   return (
     <header
       className="banner"
@@ -35,7 +37,17 @@ export default function Banner() {
         backgroundSize: 'cover',
       }}
     >
-      Banner
+      <div className="banner__contents">
+        <h1 className="banner__title">{movie.title || movie.name || movie.original_name}</h1>
+
+        <div className="banner__buttons">
+          <button className="banner__button play">Play</button>
+          <button className="banner__button info">More Information</button>
+        </div>
+
+        <h1 className="banner__description">{truncate(movie.overview, 100)}</h1>
+      </div>
+      <div className="banner--fadeBottom" />
     </header>
   );
 }
