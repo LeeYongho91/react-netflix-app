aeeff0942898e6f6b454decd85e63ac952eb0cf8
LeeYongho91 <skyks4848@naver.com>
2024-01-20 01:41:21 +0900

    라우터 추가

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 import './App.css';
-import requests from './api/requests';
-import Banner from './components/Banner';
+import { Outlet, Route, Routes } from 'react-router-dom';
 import Nav from './components/Nav';
-import Row from './components/Row';
 import Footer from './components/Footer';
+import MainPage from './pages/MainPage/index';
+import DetailPage from './pages/DetailPage/index';
+import SearchPage from './pages/SearchPage/index';
+
+function Layout() {
+  return (
+    <div>
+      <Nav />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="app">
-      <Nav />
-      <Banner />
-      <Row title="NETFLIX ORIGINALS" id="NO" fetchUrl={requests.fetchTrending} isLargeRow />
-      <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated} />
-      <Row title="Action Movies" id="AM" fetchUrl={requests.fetchActionMovies} />
-      <Row title="comedy Movies" id="CM" fetchUrl={requests.fetchComedyMovies} />
-      <Footer />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MainPage />} />
+          <Route path=":movieId" element={<DetailPage />} />
+          <Route path=":search" element={<SearchPage />} />
+          <Route />
+        </Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/MainPage/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Banner from '../../components/Banner';
+import Row from '../../components/Row';
+import requests from '../../api/requests';
+
+function MainPage() {
+  return (
+    <div>
+      <Banner />
+      <Row title="NETFLIX ORIGINALS" id="NO" fetchUrl={requests.fetchTrending} isLargeRow />
+      <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" id="AM" fetchUrl={requests.fetchActionMovies} />
+      <Row title="comedy Movies" id="CM" fetchUrl={requests.fetchComedyMovies} />
+    </div>
+  );
+}
+
+export default MainPage;
